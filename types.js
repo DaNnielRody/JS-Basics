5ae2cc94a8bfe78497f09e656a3d35d22f3f5192
dan <danielrody@gmail.com>
2024-10-14 12:19:43 -0300

    feat: js code basics with notes

---
+++ types.js
@@ -0,0 +1,21 @@
+/*
+
+No JS, temos tipos primitivos e compostos
+
+*/
+
+// Primitivos
+let string = "texto";
+let int = 12;
+let boolean = true;
+let undef; // console.log(undef) -> undefined
+let nullable = null;
+
+// Compostos
+
+array = [1, 2, 3, 4, 5]
+
+object = {
+    name: "Daniel",
+    age: 21
+}
